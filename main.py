afdb89bbf18eda16e8eb5354ee844020b3edaf5f
Martin <martinchuang1999@gmail.com>
2025-08-27 10:55:18 +0800

    Add flask

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
+from flask import Flask, request, jsonify
 from app.service.presidio_service import presidio_anonymize
 from app.components.presidio.presidio_engine import PresidioEngine
 
 # Dependency Injection - Initialize PresidioEngine once
 engine = PresidioEngine(model="mini-lm")
+app = Flask(__name__)
 
-# API
-def anonymize_text(text):
+# API endpoints
+@app.route('/')
+def hello_world():
+    response = {
+        "status": "success",
+        "body": 'Hello this is flask'
+    }
+    return jsonify(response), 200
+
+@app.route('/anonymize', methods=['POST'])
+def anonymize_text():
+    data = request.json
+    text = data.get("body", "")
     anonymized_text = presidio_anonymize(text, engine) # Execute service layer
-    return anonymized_text
+    response = {
+        "status": "success",
+        "body": anonymized_text
+    }
+    return jsonify(response), 200
 
 #Example API call
-text_input = "His name is Mr. Jones, Jones Bond and his phone number is 212-555-5555. Jones is friends with Martin"
-print(anonymize_text(text_input))
+# text_input = {"body": "His name is Mr. Jones, Jones Bond and his phone number is 212-555-5555. Jones is friends with Martin"}
+
+if(__name__) == '__main__':
+    app.run(debug=True)
