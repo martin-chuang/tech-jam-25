e674e81df3bfd1547e86a8dedc253d608dbb2a99
Chin Sek Yi <sekyichin@gmail.com>
2025-08-30 22:49:43 +0800

    fix docker compose

--- backend/app.py
+++ backend/app.py
@@ -15,4 +15,4 @@ def create_application():
 
 if __name__ == "__main__":
     app = create_application()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5050, debug=True)
